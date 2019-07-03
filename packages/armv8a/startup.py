85f7781915b137b0f547ca377f205bf8b4e4ed18
Ben Leslie <benno@brkawy.com>
2019-07-03 04:06:57 +0000

    Add basic support for single-core ARMv8A startup
    This includes the basic linker script and assembler startup
    code to bootstrap a free-standing C environment.

---
+++ packages/armv8a/startup.py
@@ -0,0 +1,35 @@
+#
+# Copyright (c) 2019, Breakaway Consulting Pty. Ltd.
+#
+
+from prj import SystemBuildError, Module
+import ply.cpp
+
+
+class EntryModule(Module):
+    xml_schema = """
+<schema>
+    <entry name="code_addr" type="int" default="0x80000" />
+    <entry name="stack_size" type="int" default="0x1000" />
+</schema>"""
+
+    files = [
+        {'input': 'startup.s', 'render': True, 'type': 'asm'},
+        {'input': 'default.ld', 'render': True, 'type': 'linker_script', 'stage': 'post_prepare'},
+    ]
+
+    def post_prepare(self, system, config):
+
+        pre_processor = ply.cpp.Preprocessor(include_paths=system.include_paths)
+
+        for c_file in system.c_files:
+            with open(c_file) as file_obj:
+                try:
+                    pre_processor.parse(file_obj.read(), c_file)
+                except ply.cpp.CppError as exc:
+                    raise SystemBuildError(str(exc))
+
+        super().post_prepare(system, config)
+
+
+module = EntryModule()  # pylint: disable=invalid-name
---
+++ packages/armv8a/startup.s
@@ -0,0 +1,60 @@
+/* Copyright (c) 2019, Breakaway Consulting Pty. Ltd. */
+.global _asm_entry
+
+.section .text.startup
+.type entry,#function
+
+_asm_entry:
+
+	/*
+	Different bootloaders start the cluster up
+	in different ways. Usually only CPU#0 would be
+	active, but in case other CPUs are active we don't
+	want them to actually do anything, so they are
+	put into an idle loop at this point.
+
+	Note: Assumption is made that bootloader has put
+	the CPU into exception level 2 (EL2).
+	*/
+
+	/*
+	Determine current CPU# (bottom 8-bits of MPIDR)
+	*/
+	MRS     x0, mpidr_el1
+	AND     x0, x0, #255
+	
+	/*
+	For CPUs other than zero, jump to the infinite loop.
+	Current implementation status only supports a single
+	CPU.
+	*/
+	CBNZ    x0, 4f
+	
+1:
+	/*
+	Clear the bss. The firmware image is usually not ELF loaded
+	but raw binary loaded, so can't trust that memory is
+	zeroed.
+	*/
+	LDR     x0, =__bss_start
+	LDR     w1, =__bss_size
+	CBZ     w1, 3f
+2:
+	STR     xzr, [x0], #8
+	SUB     w1, w1, #1
+	CBNZ    w1, 2b
+
+	/* initialise stack pointer and call main() */
+3:
+	LDR     x0, =_stack_top
+	MOV     sp, x0
+	BL      main
+
+	/*
+	Infinite loop - reached if main() returns, or for CPUs other
+	than zero
+	*/
+4:
+	WFE
+	B       4b
+   
