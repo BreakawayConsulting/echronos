153dad40b23bb417e6c9e598ec26fbd92b5dab1d
Ben Leslie <benno@brkawy.com>
2019-12-09 00:16:08 +0000

    Add armv8a.system module.
    This module contains constants and provide functionality for
    accessing system registers.
    
    Current implementation includes access to register used in
    setting up MMUs (to enable platform module to correctly
    initialized the MMUs).

---
+++ packages/armv8a/system.c
@@ -0,0 +1,20 @@
+/* Copyright (c) 2019, Breakaway Consulting Pty. Ltd. */
+/*
+ * The system module provides access to the low-level registers on the ARMv8A,
+ * and provide other system specific constants.
+ *
+ * The functionality in this module is primarily provided through the header
+ * file.
+ */
+/*<module>
+  <code_gen>template</code_gen>
+  <headers>
+      <header path="system.h" code_gen="template" />
+  </headers>
+  <schema>
+  </schema>
+</module>*/
+
+#include <stdint.h>
+
+#include "system.h"
---
+++ packages/armv8a/system.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2019, Breakaway Consulting Pty. Ltd. */
+
+static inline void write_tcr_el2(uint64_t r) {
+    asm volatile ("msr tcr_el2, %0" : : "r"(r));
+}
+
+static inline void write_mair_el2(uint64_t r) {
+    asm volatile ("msr mair_el2, %0" : : "r"(r));
+}
+
+static inline void write_ttbr0_el2(uint64_t r) {
+    asm volatile ("msr ttbr0_el2, %0" : : "r"(r));
+}
+
+static inline uint64_t read_sctlr_el2(void) {
+    uint64_t r;
+    asm volatile ("mrs %0, sctlr_el2" : "=r"(r));
+    asm volatile("dsb sy");
+    asm volatile("isb");
+
+    return r;
+}
+
+static inline void write_sctlr_el2(uint64_t r) {
+    asm volatile ("msr sctlr_el2, %0" : : "r"(r));
+}
